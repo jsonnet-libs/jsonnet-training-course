0db7e83dbb77f9d718464c8acb229b2142d563da
Duologic <jeroen@simplistic.be>
2022-05-24 23:12:40 +0200

    cleaner anchor

--- assets/anchorlinks.js
+++ assets/anchorlinks.js
@@ -10,6 +10,7 @@ document.querySelectorAll('h2, h3, h4, h5, h6').forEach($heading => {
     $heading.classList.add('anchor-heading');
 
     //create anchor
+    var $anchor;
     $anchor = document.createElement('a');
     $anchor.className = 'anchor-link';
     $anchor.href = '#' + id;
